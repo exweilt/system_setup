e5da81145705bcc4c192320599a8dff4e03478fe
exweilt <liapkalo@tcd.ie>
2024-10-21 15:41:19 +0100

    add more meat

--- system_setup.sh
+++ system_setup.sh
@@ -1,20 +1,68 @@
 !#/bin/bash
 
-$HOME_DIRECTORY="/home/lex/"
-$REPOS_DIRECTORY="${HOME_DIRECTORY}repos/"
+# TODO: Make sure to add extra and multilib repos in pacman
 
+# Constants ===================================================
+USERNAME="lex"
+$HOME_DIRECTORY="/home/${USERNAME}"
+$REPOS_DIRECTORY="${HOME_DIRECTORY}/repos"
+
+# Run As Root =================================================
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+if [ -d "${HOME_DIRECTORY}" ]; then
+    echo "The home directory <${HOME_DIRECTORY}> is present!"
+    echo "Aborting!!!"
+    exit 1
+fi
+
+# Add User ====================================================
+useradd --shell /bin/bash "${USERNAME}"
+usermod -aG wheel "${USERNAME}"
+mkdir "${HOME_DIRECTORY}" 2> /dev/null
+mkdir "${REPOS_DIRECTORY}" 2> /dev/null
+# TODO: edit sudoers file
+
+# Update System ===============================================
 pacman -Syu --noconfirm
 
+# Install base ================================================
 pacman -S base-devel git libx11 libxft xorg-server xorg-xinit terminus-font --noconfirm --needed
+pacman -S iw networkmanager --noconfirm --needed
+pacman -S exfat-utils --noconfirm --needed
 
+# Install fonts ===============================================
+pacman -S noto-fonts --noconfirm --needed
+
+# Install programs ============================================
 pacman -S tldr --noconfirm --needed
 pacman -S code --noconfirm --needed
 pacman -S micro --noconfirm --needed
 pacman -S neovim --noconfirm --needed
+pacman -S neofetch --noconfirm --needed
+pacman -S redshift --noconfirm --needed
+pacman -S nvtop --noconfirm --needed
+pacman -S htop --noconfirm --needed
+pacman -S gparted --noconfirm --needed
+pacman -S gimp --noconfirm --needed
+pacman -S zoxide --noconfirm --needed
+pacman -S alacritty --noconfirm --needed
+pacman -S flameshot --noconfirm --needed # For taking screenshots
 
-if [ -d "/home/lex/" ]; then
-    mkdir /home/lex/repos/ 2> /dev/null
-    git clone git://git.suckless.org/st "${REPOS_DIRECTORY}/st"
-    git clone git://git.suckless.org/dmenu "${REPOS_DIRECTORY}/dmenu"
-    git clone git://git.suckless.org/dwm "${REPOS_DIRECTORY}/dwm"
-fi
+git clone git://git.suckless.org/st "${REPOS_DIRECTORY}/st"
+git clone git://git.suckless.org/dmenu "${REPOS_DIRECTORY}/dmenu"
+git clone git://git.suckless.org/dwm "${REPOS_DIRECTORY}/dwm"
+git clone https://aur.archlinux.org/yay.git "${REPOS_DIRECTORY}/yay"
+git clone https://aur.archlinux.org/librewolf-bin.git "${REPOS_DIRECTORY}/librewolf"
+
+# cd "${REPOS_DIRECTORY}/st" ; makepkg -si
+# cd "${REPOS_DIRECTORY}/dmenu" ; makepkg -si
+# cd "${REPOS_DIRECTORY}/dwm" ; makepkg -si
+# cd "${REPOS_DIRECTORY}/yay" ; makepkg -si
+# cd "${REPOS_DIRECTORY}/librewolf" ; makepkg -si
+
+
+# TODO: edit sudoers file
